0bf83e9587dc2bec903776b1f1d794363b2eafa8
Mazedur Rahman <mazedur.rahman.liton@gmail.com>
2023-08-01 21:27:23 -0700

    Add simple request logging

--- src/main/java/com/example/demo/GreetingsController.java
+++ src/main/java/com/example/demo/GreetingsController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +13,8 @@ import java.util.Map;
 @RestController
 public class GreetingsController {
 
+    private static final Logger log = LoggerFactory.getLogger(GreetingsController.class);
+
     @GetMapping("/greetings")
     public ResponseEntity<Map<String, String>> greet(@RequestParam(required = false) String name) {
         HashMap<String, String> response = new HashMap<>();
@@ -18,7 +22,9 @@ public class GreetingsController {
             name = "anonymous";
         }
         response.put("message", "Hello, " + name + "!");
+
+        log.info("Received a request with name: " + name);
+
         return ResponseEntity.ok(response);
     }
 }
-
